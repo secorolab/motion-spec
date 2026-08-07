18b2b93ff5b549892070c40cced4f2ed5f9b26df
Vamsi Kalagaturu <vamsikalagaturu@gmail.com>
2026-08-07 11:14:34 +0200

    Give controller C++ one birthplace in domain_controllers.stg

--- src/motion_spec/generation/codegen.py
+++ src/motion_spec/generation/codegen.py
@@ -196,6 +196,9 @@ def generate_code(ir_path: Path, output_dir: Path, stst_bin: str):
     compile_frame_log_proto(output_dir / "frame_log.proto")
     render_template(stst_bin, "runtime_header", ir_payload_path, headers_dir / "runtime.hpp")
     render_template(
+        stst_bin, "controller_runtime_header", ir_payload_path, headers_dir / "controllers.hpp"
+    )
+    render_template(
         stst_bin, "shared_state_header", ir_payload_path, headers_dir / "shared_state.hpp"
     )
     if ir["resources"]["by_kind"]["mobile_base"]:
